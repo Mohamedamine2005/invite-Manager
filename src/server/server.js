541e7a19d8c45b6d9e7a599cc9954354adedde17
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2021-05-16 05:14:51 +0200

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,29 +1,27 @@
-const http = require("http");
 const express = require("express");
 const app = express();
+const config = require('../../config')
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 app.get("/", (req, res) => {
-  res.render("index")
+    res.render("index")
 });
 app.get("/src", (req, res) => {
-  res.render("src");
+    res.render("src");
 });
 app.get("/cmd", (req, res) => {
-  res.render("cmd");
+    res.render("cmd");
 });
 app.use(express.static(`${__dirname}/assets`));
 app.locals.basedir = `${__dirname}/assets`;
 app.use("/ping", (req, res) => {
-  res.send(new Date());
+    res.send(new Date());
 });
 app.listen(process.env.PORT || 3000);
-setInterval(() => {
-  http.get(`http://ncr-codes.glitch.me/`);
-}, 280000);
 
 const request = require("node-superfetch");
-setInterval(async () => {
-  await request.get("http://ncr-codes.glitch.me/");
-  await request.get("https://ncr-codes.glitch.me/");
+const { Http2ServerRequest } = require("http2");
+setInterval(async() => {
+    await request.get(config.express_url);
+    await request.get(config.express_url);
 }, 5 * 60 * 1000);
